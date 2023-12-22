17e6c0c89f4c90f813f1d6694e16f83923e425e1
Felix Benz-Baldas <felix.benz-baldas@atruvia.de>
2023-12-22 14:13:10 +0100

    refactor

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -19,18 +19,14 @@ public class Item {
         if (isAgedBrie) {
             if (quality < 50) {
                 quality = quality + 1;
-
             }
-
             sellIn = sellIn - 1;
-
             if (sellIn < 0) {
                 if (quality < 50) {
                     quality = quality + 1;
                 }
             }
         } else {
-
             if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (quality > 0) {
                     if (!name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -54,11 +50,9 @@ public class Item {
                     }
                 }
             }
-
             if (!name.equals("Sulfuras, Hand of Ragnaros")) {
                 sellIn = sellIn - 1;
             }
-
             if (sellIn < 0) {
                 if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (quality > 0) {
