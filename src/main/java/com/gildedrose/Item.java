49eb9efbb28d9df16882ec24428d5343ce90c49f
Felix Benz-Baldas <felix.benz-baldas@atruvia.de>
2023-12-22 14:52:03 +0100

    simplify

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -17,52 +17,45 @@ public class Item {
     void updateItem() {
         boolean isAgedBrie = name.equals("Aged Brie");
         boolean isBackstagePass = name.equals("Backstage passes to a TAFKAL80ETC concert");
+        boolean isSulfuras = name.equals("Sulfuras, Hand of Ragnaros");
         if (isAgedBrie) {
             if (quality < 50) {
-                quality = quality + 1;
+                quality++;
             }
             sellIn = sellIn - 1;
             if (sellIn < 0) {
                 if (quality < 50) {
-                    quality = quality + 1;
+                    quality++;
                 }
             }
+        } else if (isBackstagePass) {
+            if (quality < 50) {
+                quality++;
+                if (sellIn < 11) {
+                    if (quality < 50) {
+                        quality++;
+                    }
+                }
+                if (sellIn < 6) {
+                    if (quality < 50) {
+                        quality++;
+                    }
+                }
+            }
+            sellIn--;
+            if (sellIn < 0) {
+                quality = 0;
+            }
+        } else if (isSulfuras) {
+            // do nothing
         } else {
-            if (isBackstagePass) {
-                if (quality < 50) {
-                    quality = quality + 1;
-
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-                }
-                sellIn = sellIn - 1;
-                if (sellIn < 0) {
-                    quality = 0;
-                }
-            } else {
+            if (quality > 0) {
+                quality--;
+            }
+            sellIn--;
+            if (sellIn < 0) {
                 if (quality > 0) {
-                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                        quality = quality - 1;
-                    }
-                }
-                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                    sellIn = sellIn - 1;
-                }
-                if (sellIn < 0) {
-                    if (quality > 0) {
-                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                            quality = quality - 1;
-                        }
-                    }
+                    quality--;
                 }
             }
         }
