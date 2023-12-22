38d4c1782143f64731b6e329f85c190e1266fc54
Felix Benz-Baldas <felix.benz-baldas@atruvia.de>
2023-12-22 15:14:59 +0100

    divide test code and production code

--- src/test/java/com/gildedrose/GildedRoseApprovalTest.java
+++ src/test/java/com/gildedrose/GildedRoseApprovalTest.java
@@ -8,10 +8,35 @@ import org.approvaltests.Approvals;
 import org.approvaltests.reporters.DiffReporter;
 import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.Test;
-import org.w3c.dom.Text;
 
 @UseReporter(DiffReporter.class)
 public class GildedRoseApprovalTest {
+
+    private void legacyTestCode(String... args) {
+        Item[] items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20),
+                new Item("Aged Brie", 2, 0),
+                new Item("Elixir of the Mongoose", 5, 7),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", 3, 6) };
+
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < 31; i++) {
+            System.out.println("-------- day " + i + " --------");
+            System.out.println("name, sellIn, quality");
+            for (int j = 0; j < items.length; j++) {
+                System.out.println(items[j]);
+            }
+            System.out.println("");
+            app.updateQuality();
+        }
+    }
 
     @Test
     public void thirtyDays() {
@@ -20,7 +45,7 @@ public class GildedRoseApprovalTest {
         System.setOut(new PrintStream(fakeoutput));
         System.setIn(new ByteArrayInputStream("a\n".getBytes()));
 
-        Program.main();
+        legacyTestCode();
         String output = fakeoutput.toString();
 
         Approvals.verify(output);
